e59fc5572f0a779d58ba010cdb6785fbc8c6cfb6
Keks <keks@htmlacademy.ru>
2022-03-26 17:25:54 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,5 @@
+import './data.js';
+import './util.js';
+import './rendering-miniatures.js';
+import './full-screen-picture.js';
+import './form-validation.js';
---
+++ 9/js/util.js
@@ -0,0 +1,33 @@
+const checkCommentLength = 140;
+const countComment = function (userComment) {
+  if (userComment.length <= checkCommentLength) {
+    return true;
+  } else {
+    Error('Максимальное количество символов комментария не более 140');
+    return false;
+  }
+};
+countComment('Комментарий посетителя сайта');
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (min < 0 || max < 0 || max === min || min === max || max < min || min > max) {
+    Error('Вводите целые положительные числа от меньшего к большему');
+  } else {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+}
+
+const getRandomArrayObject = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
+
+/* const isEscapeKey = (evt) => {
+ return evt.key === 'Escape';
+};
+
+const isEnterKey = (evt) => {
+  return evt.key === 'Enter';
+}; */
+
+export {getRandomArrayObject, getRandomIntInclusive, countComment};
+
